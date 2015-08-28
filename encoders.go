ff5a85fd9b46383651e65d1d443709dd78ddbf83
Nick Gauthier <ngauthier@gmail.com>
2015-08-28 08:48:16 -0400

    remove unused error and check from encoder

--- encoders.go
+++ encoders.go
@@ -1,22 +1,14 @@
 package ledge
 
-import (
-	"errors"
-	"io"
-)
+import "io"
 
 var (
 	rpcEncoderInstance = &rpcEncoder{}
-
-	separator                      = byte('\n')
-	errShouldNotBeWritingZeroBytes = errors.New("ledge: should not be writing 0 bytes")
+	separator          = byte('\n')
 )
 
 type rpcEncoder struct{}
 
 func (r *rpcEncoder) Encode(writer io.Writer, data []byte) (int, error) {
-	if len(data) == 0 {
-		return 0, nil
-	}
 	return writer.Write(append(data, separator))
 }
